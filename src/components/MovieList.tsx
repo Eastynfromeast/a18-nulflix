6b96f78cf003336d141f5f5f667342e855d2cf36
Eastyn <eastynwork@gmail.com>
2024-07-20 18:03:25 +0900

    To let staggerChildren work, we need to remove hover animation on the children's variants

---
+++ src/components/MovieList.tsx
@@ -0,0 +1,124 @@
+import styled from "styled-components";
+import { AnimatePresence, motion } from "framer-motion";
+import { IDataResults, IMovie } from "../utils/types";
+import { makeImagePath } from "../utils/api";
+import MovieCard from "./MovieCard";
+import { useState } from "react";
+
+const MovieGrid = styled(motion.ul)`
+	display: grid;
+	width: 100%;
+	grid-template-columns: repeat(auto-fill, minmax(80px, 280px));
+	gap: 15px;
+	justify-content: center;
+`;
+
+const Movie = styled(motion.li)`
+	overflow: hidden;
+	border-radius: 25px;
+	position: relative;
+`;
+
+const Thumbnail = styled(motion.img)`
+	width: 100%;
+	border-radius: 25px;
+`;
+
+const MovieTitle = styled(motion.div)`
+	display: flex;
+	flex-direction: column;
+	justify-content: flex-end;
+	padding: 30px 15px;
+	width: 100%;
+	height: 100%;
+	position: absolute;
+	top: 0;
+	left: 0;
+	background: linear-gradient(to bottom, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.75));
+	font-size: 16px;
+	h3 {
+		font-weight: 600;
+		font-size: 1.8em;
+	}
+	p {
+		display: flex;
+		flex-direction: row;
+		justify-content: flex-start;
+		align-items: center;
+		margin-bottom: 5px;
+		font-size: 1.2em;
+	}
+	opacity: 0;
+`;
+
+const Star = styled.svg`
+	max-width: 1.25em;
+	margin-right: 5px;
+`;
+
+const moviesVarianst = {
+	initial: { opacity: 0, y: 50 },
+	animate: {
+		opacity: 1,
+		y: 1,
+		transition: {
+			type: "spring",
+			staggerChildren: 0.3,
+			delayChildren: 0.3,
+			duration: 1,
+		},
+	},
+};
+
+const movieVariants = {
+	initial: { opacity: 0, scaleY: 0 },
+	animate: { opacity: 1, scaleY: 1, transition: { duration: 0.3 } },
+};
+
+const titleVariants = {
+	hover: { opacity: 1, transition: { delay: 0.2 } },
+};
+
+function MovieList(data: IDataResults) {
+	const [clickedMovie, setClickedMovie] = useState<null | number>(null);
+	const onClickMovie = (id: number) => {
+		setClickedMovie(id);
+	};
+	const closeModal = () => setClickedMovie(null);
+
+	return (
+		<>
+			<AnimatePresence>
+				<MovieGrid variants={moviesVarianst} initial="initial" animate="animate">
+					{data.results.slice(1).map((movie: IMovie, index) => (
+						<Movie
+							layoutId={movie.id + ""}
+							variants={movieVariants}
+							whileHover={{ y: -25, scale: 1.15, zIndex: 99, transition: { type: "spring", duration: 0.3 } }}
+							key={movie.id}
+							onClick={() => onClickMovie(movie.id)}
+						>
+							<Thumbnail src={makeImagePath(movie.poster_path)} alt={movie.title} />
+							<MovieTitle variants={titleVariants} whileHover="hover">
+								<p>
+									<Star fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+										<path
+											clipRule="evenodd"
+											fillRule="evenodd"
+											d="M10.868 2.884c-.321-.772-1.415-.772-1.736 0l-1.83 4.401-4.753.381c-.833.067-1.171 1.107-.536 1.651l3.62 3.102-1.106 4.637c-.194.813.691 1.456 1.405 1.02L10 15.591l4.069 2.485c.713.436 1.598-.207 1.404-1.02l-1.106-4.637 3.62-3.102c.635-.544.297-1.584-.536-1.65l-4.752-.382-1.831-4.401Z"
+										/>
+									</Star>
+									{movie.vote_average.toFixed(2)}
+								</p>
+								<h3>{movie.title}</h3>
+							</MovieTitle>
+						</Movie>
+					))}
+				</MovieGrid>
+				{clickedMovie && <MovieCard onOverlayClick={closeModal} clickedMovie={clickedMovie} />}
+			</AnimatePresence>
+		</>
+	);
+}
+
+export default MovieList;
--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,10 +1,9 @@
 import { useQuery } from "@tanstack/react-query";
-import { getMovie, getPopular, makeBgPath, makeImagePath } from "../utils/api";
-import { IDataResults, IMovie } from "../utils/types";
+import { getPopular, makeImagePath } from "../utils/api";
+import { IDataResults } from "../utils/types";
 import styled from "styled-components";
 import { motion } from "framer-motion";
-import { useEffect, useState } from "react";
-import MovieCard from "../components/MovieCard";
+import MovieList from "../components/MovieList";
 
 const Container = styled.div`
 	position: relative;
@@ -17,103 +16,11 @@ const MainVisual = styled(motion.div)`
 	text-align: center;
 `;
 
-const MovieGrid = styled(motion.ul)`
-	display: grid;
-	width: 100%;
-	grid-template-columns: repeat(auto-fill, minmax(80px, 280px));
-	gap: 15px;
-	justify-content: center;
-`;
-
-const Movie = styled(motion.li)`
-	overflow: hidden;
-	border-radius: 25px;
-	text-align: center;
-	img {
-		width: 100%;
-	}
-`;
-
-const Overlay = styled(motion.div)<{ $bgPhoto: string }>`
-	display: flex;
-	justify-content: center;
-	align-items: center;
-	gap: 15px;
-	width: 100%;
-	height: 100%;
-	position: fixed;
-	top: 0;
-	background: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(${props => props.$bgPhoto}) no-repeat;
-	background-size: cover;
-`;
-
-const Card = styled(motion.div)`
-	overflow: hidden;
-	display: flex;
-	flex-direction: column;
-	color: ${props => props.theme.textColor};
-	width: 50%;
-	height: 50%;
-	max-width: 480px;
-	min-height: 720px;
-	border-radius: 25px;
-	img {
-		width: 100%;
-		object-fit: cover;
-	}
-`;
-
-const PosterCard = styled(Card)<{ $bgPhoto: string }>`
-	background-image: url(${props => props.$bgPhoto});
-`;
-
-const ContextCard = styled(Card)`
-	display: flex;
-	flex-direction: column;
-	justify-content: center;
-	gap: 7px;
-	padding: 0 20px 50px;
-	font-size: 16px;
-	word-break: keep-all;
-	background-color: rgba(0, 0, 0, 0.75);
-	h2 {
-		font-weight: 600;
-		font-size: 2.5em;
-		margin-bottom: 15px;
-	}
-`;
-
-const Genres = styled.ul`
-	display: flex;
-	flex-direction: row;
-	justify-content: flex-start;
-	align-items: center;
-	width: 100%;
-	gap: 10px;
-`;
-
-const SmallTitle = styled.span`
-	display: block;
-	width: 100%;
-	font-weight: 500;
-	opacity: 0.75;
-	margin-top: 15px;
-`;
-
-const Overview = styled.p`
-	line-height: 1.35;
-`;
-
 function Home() {
 	const { data, isLoading } = useQuery<IDataResults>({
 		queryKey: ["popular", ""],
 		queryFn: () => getPopular(),
 	});
-	const [clickedMovie, setClickedMovie] = useState<null | number>(null);
-	const onClickMovie = (id: number) => {
-		setClickedMovie(id);
-	};
-	const closeModal = () => setClickedMovie(null);
 
 	return (
 		<Container>
@@ -125,15 +32,7 @@ function Home() {
 						<img src={makeImagePath(data.results[0].poster_path)} alt={data.results[0].title} />
 						<div> {data.results[0].title}</div>
 					</MainVisual>
-					<MovieGrid>
-						{data.results.slice(1).map((movie: IMovie) => (
-							<Movie layoutId={movie.id + ""} key={movie.id} onClick={() => onClickMovie(movie.id)}>
-								<img src={makeImagePath(movie.poster_path)} alt={movie.title} />
-								<h3>{movie.title}</h3>
-							</Movie>
-						))}
-					</MovieGrid>
-					{clickedMovie && <MovieCard onOverlayClick={closeModal} clickedMovie={clickedMovie} />}
+					<MovieList {...data} />
 				</>
 			)}
 		</Container>
